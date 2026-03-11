54c73dad438d45f14b8402fb41933488b561a67b
sunattah <sundayattah319@gmail.com>
2026-03-11 14:45:53 +0000

    Create average.go

---
+++ average.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var studentName = "samson"
+	var subject1 = 43
+	var subject2 = 23
+	var subject3 = 12
+	var sum = 78 / 2
+	fmt.Println(studentName)
+	fmt.Println(subject1 + subject2 + subject3)
+	fmt.Println(sum)
+
+}
